6c847eedb6401fe697c9ebd598e3c410b4b08f8f
Richard F Jones <richardnz@outlook.com>
2024-02-06 03:55:22 +0000

    updated Moodle version

--- version.php
+++ version.php
@@ -29,7 +29,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'mod_simplelesson';
 $plugin->version = 2024020601;
 $plugin->release = 'v2.06.';
-$plugin->requires = 2023100902;
+$plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron = 0;
 $plugin->dependencies = [];
